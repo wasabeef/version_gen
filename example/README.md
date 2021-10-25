ca5e8d5fed29d09ba3f2b15f63b8ef89c784400d
wasabeef <dadadada.chop@gmail.com>
2021-10-25 19:36:42 +0900

    feat: create version_gen

---
+++ example/README.md
@@ -0,0 +1,10 @@
+# example
+
+A sample project using FlutterGen.
+
+## Getting Started
+
+```
+cd ..
+melos run pub:get
+```
---
+++ test/versioin_gen_test.dart
@@ -0,0 +1,41 @@
+@TestOn('vm')
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:version_gen/version_gen.dart';
+
+void main() {
+  test('Change output path', () async {
+    const pubspec = 'test_resources/pubspec.yaml';
+    const generatedPath = 'test_resources/lib/gen/';
+    const generatedFileName = 'version.gen.dart';
+    const fact = 'test_resources/actual_data/version.gen.dart';
+
+    VersionGenBuilder().generate(
+      pubspec: pubspec,
+      genFile: generatedFileName,
+    );
+    final actual = File('$generatedPath/$generatedFileName')
+        .readAsStringSync()
+        .replaceAll('\r\n', '\n');
+    final expected = File(fact).readAsStringSync().replaceAll('\r\n', '\n');
+    expect(actual, expected);
+  });
+
+  test('Default output path', () async {
+    const pubspec = 'test_resources/pubspec_not_settings.yaml';
+    const generatedPath = 'test_resources/aaa/bbb/ccc/';
+    const generatedFileName = 'version.gen.dart';
+    const fact = 'test_resources/actual_data/version.gen.dart';
+
+    VersionGenBuilder().generate(
+      pubspec: pubspec,
+      genFile: generatedFileName,
+    );
+    final actual = File('$generatedPath/$generatedFileName')
+        .readAsStringSync()
+        .replaceAll('\r\n', '\n');
+    final expected = File(fact).readAsStringSync().replaceAll('\r\n', '\n');
+    expect(actual, expected);
+  });
+}
